ac818bcda62bb8b6ba141ccc2b1c4912fec85714
Kashvi Rita <143584197+Kashvirita@users.noreply.github.com>
2025-05-04 21:19:58 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+const API = import.meta.env.VITE_API_BASE_URL;
 
 const Login = () => {
   const [committeeName, setCommitteeName] = useState('');
@@ -12,7 +13,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
+      const res = await axios.post(`${API}/api/auth/login`, {
         committeeName,
         facultyEmail,
         password
